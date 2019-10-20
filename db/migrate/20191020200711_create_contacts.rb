629d6bd19d722fca0d06ba3829f5e0f23030c7d7
kjdub01 <kjewettwilliams@gmail.com>
2019-10-20 13:41:52 -0700

    intial user controller

--- db/migrate/20191020200711_create_contacts.rb
+++ db/migrate/20191020200711_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
       t.string :name
-      t.number :phone
+      t.string :phone
       t.string :email
       t.timestamps
     end
