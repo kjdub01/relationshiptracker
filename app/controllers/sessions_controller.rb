4aac30d64d502d9d45988980864b2063e5cd55cf
kjdub01 <kjewettwilliams@gmail.com>
2019-10-20 12:52:31 -0700

    add logice to create login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect_to user_path(@user)
+    else
+      flash[:danger] = "Email or Password is invalid"
+      render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end
